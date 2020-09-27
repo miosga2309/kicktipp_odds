a01c2903e06c75964e6c9733ea1aa39095681000
Jonas Miosga <jonas.miosga@uni-konstanz.de>
2020-09-27 21:04:22 +0200

    data organised into pandas table

--- odds_extractor.py
+++ odds_extractor.py
@@ -1,44 +1,24 @@
 import json
 import requests
+import numpy as np
+import pandas as pd
+from datetime import datetime
 
-# An api key is emailed to you when you sign up to a plan
+# API key
 key_obj = open('/Users/jonasmiosga/Desktop/kicktipp_odds/key.txt', 'r')
 key_line = key_obj.readlines()
 api_key = str(key_line[0])
 api_key = api_key.rstrip()
 
-# First get a list of in-season sports
-sports_response = requests.get('https://api.the-odds-api.com/v3/sports', params={
-    'api_key': api_key
-})
 
-sports_json = json.loads(sports_response.text)
-
-if not sports_json['success']:
-    print(
-        'There was a problem with the sports request:',
-        sports_json['msg']
-    )
-
-else:
-    print()
-    print(
-        'Successfully got {} sports'.format(len(sports_json['data'])),
-        'Here\'s the first sport:'
-    )
-    print(sports_json['data'][0])
-
-
-
-# To get odds for a sepcific sport, use the sport key from the last request
-#   or set sport to "upcoming" to see live and upcoming across all sports
+# only retrieve Bundesliga data
 sport_key = 'soccer_germany_bundesliga'
 
 odds_response = requests.get('https://api.the-odds-api.com/v3/odds', params={
     'api_key': api_key,
     'sport': sport_key,
-    'region': 'eu', # uk | us | eu | au
-    'mkt': 'h2h' # h2h | spreads | totals
+    'region': 'eu',
+    'mkt': 'h2h' # h2h (head to head)
 })
 
 odds_json = json.loads(odds_response.text)
@@ -49,17 +29,66 @@ if not odds_json['success']:
     )
 
 else:
-    # odds_json['data'] contains a list of live and
-    #   upcoming events and odds for different bookmakers.
-    # Events are ordered by start time (live events are first)
+    # odds_json['data'] contains a list of live and upcoming events and odds
+    all_lists = []
+
+    for i in range(0,len(odds_json['data'])):
+        ind_list = []
+        ts = odds_json['data'][i]['commence_time']
+        ind_list.append(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d'))
+        home_t = odds_json['data'][i]['home_team']
+        ind_list.append(home_t)
+        ind_list.append("".join(set(odds_json['data'][i]['teams'])-set([home_t])))
+
+        multiple_lists = []
+        for j in range(0,len(odds_json['data'][i]['sites'])):
+            multiple_lists.append(odds_json['data'][i]['sites'][j]['odds']['h2h'])
+        arrays = [np.array(x) for x in multiple_lists]
+        mean_odds = [np.mean(k) for k in zip(*arrays)]
+        [ind_list.append(round(x,2)) for x in mean_odds]
+        ind_list.append(odds_json['data'][i]['sites_count'])
+        all_lists.append(ind_list)
+
+    df = pd.DataFrame(all_lists, columns = ['date', 'home_team', 'opponent',
+                                            '1', 'X', '2','n_providers'])
+    print(df)
+
+    # check usage
+    print()
+    print('Remaining requests', odds_response.headers['x-requests-remaining'])
+    print('Used requests', odds_response.headers['x-requests-used'])
+
+
+
+
+#### appendix ####
+# get a list of in-season sports
+switch = 0
+if switch == 1:
+    sports_response = requests.get('https://api.the-odds-api.com/v3/sports', params={
+        'api_key': api_key
+    })
+
+    sports_json = json.loads(sports_response.text)
+
+    if not sports_json['success']:
+        print(
+            'There was a problem with the sports request:',
+            sports_json['msg']
+        )
+
+    else:
+        print()
+        print(
+            'Successfully got {} sports'.format(len(sports_json['data'])),
+            'Here\'s the first sport:'
+        )
+        print(sports_json['data'][0])
+
+    ## event import message
     print()
     print(
         'Successfully got {} events'.format(len(odds_json['data'])),
         'Here\'s the first event:'
     )
     print(odds_json['data'][0])
-
-    # Check your usage
-    print()
-    print('Remaining requests', odds_response.headers['x-requests-remaining'])
-    print('Used requests', odds_response.headers['x-requests-used'])
