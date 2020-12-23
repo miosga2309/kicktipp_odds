f65f48ec72a1f31de6dbe1a94f85ef918e015816
Jonas Miosga <jonas.miosga@uni-konstanz.de>
2020-12-23 15:11:50 +0100

    class structure added

---
+++ scripts/test1.py
@@ -0,0 +1,3 @@
+import update_df
+
+update_df.update_df()
--- scripts/update_df.py
+++ scripts/update_df.py
@@ -1,18 +1,22 @@
-import pandas as pd
+def update_df():
+    import pandas
 
-# load latest dataset
-df = pd.read_csv("/Users/jonasmiosga/Desktop/kicktipp_odds/data/df.csv", encoding = 'utf-8')
+    # load latest dataset
+    df = pandas.read_csv("/Users/jonasmiosga/Desktop/kicktipp_odds/data/df.csv", encoding = 'utf-8')
 
-# download newest version of this season's dataset
-new = pd.read_csv("http://www.football-data.co.uk/mmz4281/2021/D1.csv", encoding = 'utf-8')
+    # download newest version of this season's dataset
+    new = pandas.read_csv("http://www.football-data.co.uk/mmz4281/2021/D1.csv", encoding = 'utf-8')
 
-# find equal variables
-col_list = list(set(list(df)).intersection(list(new)))
+    # find equal variables
+    col_list = list(set(list(df)).intersection(list(new)))
 
-# filter dataframes by common variables
-df = df.filter(items=col_list)
-new = new.filter(items=col_list)
+    # filter dataframes by common variables
+    df = df.filter(items=col_list)
+    new = new.filter(items=col_list)
 
-# save new updated dataset
-df = pd.concat([df,new]).drop_duplicates(subset='Date').reset_index(drop=True)
-df.to_csv('/Users/jonasmiosga/Desktop/kicktipp_odds/data/df.csv', index=False, encoding = 'utf-8')
+    # save new updated dataset
+    df = pandas.concat([df,new]).drop_duplicates(subset='Date').reset_index(drop=True)
+    df.to_csv('/Users/jonasmiosga/Desktop/kicktipp_odds/data/df.csv', index=False, encoding = 'utf-8')
+
+if __name__ == '__main__':
+    update_df()
